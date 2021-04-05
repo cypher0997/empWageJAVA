a7849c3d98b7b41dcbf3c4de5de274cf48b5c0f4
testForMe-Now <81403199+testForMe-Now@users.noreply.github.com>
2021-04-05 09:15:18 +0530

    Update empCDWSwitch.java

--- empCDWSwitch.java
+++ empCDWSwitch.java
@@ -1,16 +1,20 @@
 
+
 public class empCDWSwitch{
+	public static final int isFulltime=1;
+	public static final int isParttime=2;
+
 	public static void main( String args[]){
 		int empRatePerHr=20;
 		double empCheck=Math.floor(Math.random()*10)%3;
 		int value = (int) empCheck;
 		int empHr=0;
 		switch (value){
-			case 1:
-				empHr=4;
-				break;
-			case 2:
+			case isFulltime:
 				empHr=8;
+				break;
+			case isParttime:
+				empHr=4;
 				break;
 			case 3:
 				empHr=0;
